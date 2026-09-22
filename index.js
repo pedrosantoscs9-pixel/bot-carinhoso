b603dd05821d7af3ae22e184da72fb4905e6a366
pedrosantoscs9-pixel <pedrosantos.cs9@gmail.com>
2026-09-22 10:07:16 -0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Bot Carinhoso ON'));
-app.listen(process.env.PORT || 3000, () => console.log('Site fake on'));
+app.get('/', (req, res) => res.send('Granoleiro ta online!'));
+app.listen(process.env.PORT || 10000, () => {
+  console.log('Servidor web ligado');
+});
 const {
   ActionRowBuilder,
   ButtonBuilder,
